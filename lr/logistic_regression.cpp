9b35d618af985c41ff602b79839ab8c779a5ede7
hongyanee@gmail.com <hongyanee@gmail.com>
2019-12-19 10:30:13 +0800

    output average delta loss

--- lr/logistic_regression.cpp
+++ lr/logistic_regression.cpp
@@ -5,7 +5,6 @@
 #include <set>
 #include <map>
 #include <algorithm>
-#include <thread>
 #include <math.h>
 #include <vector>
 #include <stdio.h>
@@ -20,11 +19,11 @@ using std::endl;
 //////////////////// 配置项。 /////////////
 // notes: 未来这些配置和方法都封装在类中
 // 停止条件1  loss <= epsilon
-double epsilon = 1e-4;
+double epsilon = 1e-5;
 // 停止条件2  iter < maxIters
 int maxIters = 100;
 // 学习率
-double learnRate = 0.01;
+double learnRate = 0.10;
 // 正则化系数 L1 系数
 double lambda1 = 0.00001;
 // 正则化系数 L2 系数
@@ -50,7 +49,6 @@ double sigmoid(std::vector<int> &x, std::vector<double> &w)
 
 void parallelLR(vector<vector<int> > &X, vector<double> &y, vector<double> &w) 
 {
-    std::vector<std::thread> trainWorkers = std::vector<std::thread>(threads);
     int miniBatchCount = maxIters * X.size() / batchSize / threads;  // 最大循环轮次
     for (int iter = 0; iter < miniBatchCount; iter++) {
         
@@ -68,7 +66,8 @@ void lrTrainMinibatch() {
  * @param vector<double> 训练样本 label
  * @param vector<double> 模型参数
  */
-void discreteLR(vector<vector<int> > &X, vector<double> &y, vector<double> &w)
+void discreteLR(vector<vector<int> > &X, vector<double> &y, vector<double> &w, 
+		std::map<std::string, int> &features, const char* pathModel)
 {
 
     // Batch Gradient Desent
@@ -77,11 +76,14 @@ void discreteLR(vector<vector<int> > &X, vector<double> &y, vector<double> &w)
         std::cout << "==============================================" << std::endl;
         std::cout << "training iter " << iter << " start..." << std::endl;
         size_t batchIter = 0;
-        size_t xs = 0;
+		size_t xs = 0;
         size_t xe = xs + batchSize;
         double deltaLoss = 0.0;
+        double avgDeltaLoss = 0.0;
+		size_t avgDeltaLossWindow = 100;
         while (xs < X.size() - 1)
         {
+			batchIter ++;
             if (xe >= X.size())
             {
                 xe = X.size();
@@ -107,77 +109,78 @@ void discreteLR(vector<vector<int> > &X, vector<double> &y, vector<double> &w)
                 double wj_gradient = predict_gd + lambda1_gd + lambda2_gd;
                 // 更新梯度
                 w[wj] = w[wj] - learnRate * wj_gradient;
-                deltaLoss += abs(wj_gradient);
+                deltaLoss += (wj_gradient > 0 ? wj_gradient : -wj_gradient);
             } // end wj
             xs = xe;
             xe = xe + batchSize;
             deltaLoss = deltaLoss / static_cast<double>(w.size());
-            if (batchIter % 100 == 0)
+			avgDeltaLoss += deltaLoss;
+            if (batchIter % avgDeltaLossWindow == 0)
             {
-                std::cout << "training minibatch done. delta loss = " << deltaLoss << std::endl;
+                std::cout << "training minibatch done. average delta loss = " << (avgDeltaLoss / avgDeltaLossWindow) << std::endl;
+				avgDeltaLoss = 0.0;
             }
             if (deltaLoss < epsilon)
             {
                 return;
             }
-            deltaLoss = 0.0;
+			deltaLoss = 0.0;
         } // end batchIter
         // 一轮结束输出相关日志
         std::cout << "training iter " << iter << " finished." << std::endl;
-        // 计算前 10000 个样本的 loss
-        double predictLoss = 0.0;
-        size_t predictCount = X.size();
-        for (int xi = 0; xi < predictCount; ++xi)
-        {
-            double y_hat = sigmoid(X[xi], w);
-            predictLoss = predictLoss - y[xi] * log(y_hat) - (1 - y[xi]) * log(1 - y_hat);
-        }
-        predictLoss = predictLoss / predictCount;
-        double regularLoss = 0.0;
-        for (int wj = 0; wj < w.size(); ++wj)
-        {
-            regularLoss += (lambda1 * (w[wj] > 0 ? w[wj] : -w[wj]));
-            regularLoss += (lambda2 * w[wj] * w[wj]);
-        }
-        std::cout << "total loss = " << predictLoss + regularLoss
-                  << "; predict loss = " << predictLoss
-                  << "; regular loss = " << regularLoss << std::endl;
-        if (predictLoss + regularLoss < epsilon)
-            break;
+		outputModel(pathModel, w, features);
     }
+	// 退出前计算所有样本的Loss
+	double predictLoss = 0.0;
+	size_t predictCount = X.size();
+	for (int xi = 0; xi < predictCount; ++xi)
+	{
+		double y_hat = sigmoid(X[xi], w);
+		predictLoss = predictLoss - y[xi] * log(y_hat) - (1 - y[xi]) * log(1 - y_hat);
+	}
+	predictLoss = predictLoss / predictCount;
+	double regularLoss = 0.0;
+	for (int wj = 0; wj < w.size(); ++wj)
+	{
+		regularLoss += (lambda1 * (w[wj] > 0 ? w[wj] : -w[wj]));
+		regularLoss += (lambda2 * w[wj] * w[wj]);
+	}
+	std::cout << "total loss = " << predictLoss + regularLoss
+		<< "; predict loss = " << predictLoss
+		<< "; regular loss = " << regularLoss << std::endl;
 }
 
 int main(int argc, char *argv[])
 {
-    if (argc != 4)
-    {
-        cout << "Usage: " << argv[0] << " features_dict instances_file model_file" << std::endl;
-        return -1;
-    }
-    std::map<std::string, int> features;
-    std::vector<std::vector<int>> X;
-    std::vector<double> y;
-    int maxFeaIdx = 0;
-    loadFeatures(argv[1], features, maxFeaIdx);
-    loadInstances(argv[2], X, y);
+	if (argc != 4)
+	{
+		cout << "Usage: " << argv[0] << " features_dict instances_file model_file" << std::endl;
+		return -1;
+	}
+	std::map<std::string, int> features;
+	std::vector<std::vector<int> > X;
+	std::vector<double> y;
+	int maxFeaIdx = 0;
+	loadFeatures(argv[1], features, maxFeaIdx);
+	loadInstances(argv[2], X, y);
 
-    // 增加 Bias 特征
-    int baisFeaIdx = maxFeaIdx + 1;
-    features.insert(std::make_pair("Bias", baisFeaIdx));
-    for (int xi = 0; xi < X.size(); ++xi)
-    {
-        X[xi].push_back(baisFeaIdx);
-    }
+	// 增加 Bias 特征
+	int baisFeaIdx = maxFeaIdx + 1;
+	features.insert(std::make_pair("Bias", baisFeaIdx));
+	for (int xi = 0; xi < X.size(); ++xi)
+	{
+		X[xi].push_back(baisFeaIdx);
+	}
 
-    std::vector<double> weights;
-    for (int i = 0; i < features.size(); ++i)
-    {
-        double randWeight = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
-        weights.push_back(randWeight);
-    }
-    cout << "start training: feature size = " << features.size()
-         << "; instances count = " << X.size()
-         << "; create weights = " << weights.size() << std::endl;
-    discreteLR(X, y, weights);
-    outputModel(argv[3], weights, features);
+	std::vector<double> weights;
+	for (int i = 0; i < features.size(); ++i)
+	{
+		double randWeight = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+		weights.push_back(randWeight);
+	}
+	cout << "start training: feature size = " << features.size()
+		<< "; instances count = " << X.size()
+		<< "; create weights = " << weights.size() << std::endl;
+	const char* pathModel = argv[3];
+	discreteLR(X, y, weights, features, pathModel);
 }
